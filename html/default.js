d143593070b0f00a8cd93a2c5b8cdd1a5976afbf
ahhn <djahhn@gmail.com>
2015-02-28 15:37:09 +0900

    day06 final commit.

--- html/default.js
+++ html/default.js
@@ -1,6 +1,14 @@
 jQuery(function($){
-	$(".gnb>button").on("click", function(){
-		//$(".gnb>ul").slideToggle(200);
+	var $gnbBtn = $(".gnb>button");
+	$gnbBtn.on("click", function(){
 		$(this).next().slideToggle(200);
+
+		if( $gnbBtn.text() === "메뉴 열기" ){
+			// 메뉴 닫기 텍스트 표시
+			$gnbBtn.text("메뉴 닫기");
+		} else {
+			// 메뉴 열기 텍스트 표시
+			$gnbBtn.text("메뉴 열기");
+		}
 	});
 });
