01405fda15264726426b89c34a337033d6789df7
ahhn <djahhn@gmail.com>
2015-03-07 14:01:50 +0900

    day08 mid-commit.

--- html/default.js
+++ html/default.js
@@ -1,5 +1,7 @@
 jQuery(function($){
+	// 모바일 GNB 토글 버튼
 	var $gnbBtn = $(".gnb>button");
+	
 	$gnbBtn.on("click", function(){
 		$(this).next().slideToggle(200);
 
@@ -10,5 +12,10 @@ jQuery(function($){
 			// 메뉴 열기 텍스트 표시
 			$gnbBtn.text("메뉴 열기");
 		}
+	});
+
+	// 태블릿 + 데스크톱 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover focus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
 	});
 });
